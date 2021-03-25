cf9d76c9bee3816fa633dc67af2b7e7ee4326d92
NTkot <kotarelasn@yahoo.com>
2021-03-25 02:34:28 +0200

    added ouster train data to examples

--- src/main_ouster.cpp
+++ src/main_ouster.cpp
@@ -15,8 +15,8 @@ int main(int argc, char * argv[]) {
     } else {
         std::string package_share_path = ament_index_cpp::get_package_share_directory("turtle_lidar");
         std::string pathToConfigFile = package_share_path + "/lidarConfig.ini";
-        std::string pathToTrainXFile = package_share_path + "/simConeTrainDataX.txt";
-        std::string pathToTrainYFile = package_share_path + "/simConeTrainDataY.txt";
+        std::string pathToTrainXFile = package_share_path + "/ousterConeTrainDataX.txt";
+        std::string pathToTrainYFile = package_share_path + "/ousterConeTrainDataY.txt";
         rclcpp::spin(std::make_shared<OusterDriver>(pathToConfigFile, pathToTrainXFile, pathToTrainYFile));
     }
     rclcpp::shutdown();
